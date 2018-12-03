c8f9cf35f85f1c16763ff9ddab32fc5cb9696df6
CAmper18 <45084295+CAmper18@users.noreply.github.com>
2018-12-03 17:52:56 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,34 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+
+
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('-bc-users')){
+if(!message.author.id === '489379054584201247') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`!bc | Netflex`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
+
+client.login(process.env.BOT_TOKEN);
